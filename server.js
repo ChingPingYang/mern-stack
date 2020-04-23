af1c4f52440be24bd3f7756fc95767697ab45960
JamesYang <gn00999829@gmail.com>
2020-04-23 13:56:47 -0700

    final

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const server = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = express('path');
 
 
 // @ Connect Database
@@ -18,6 +19,15 @@ server.use('/api/auth', require('./routes/api/auth'));
 server.use('/api/posts', require('./routes/api/posts'));
 server.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    server.use(express.static('client/build'));
+
+    server.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+    })
+}
 
 server.listen(PORT, ()=> {
     console.log(`server started at port ${PORT}`);
